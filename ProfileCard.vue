eb99569feff19e0ed43dcd19c4f7a8625ca8770d
dmitry-ios <60504537+dmitry-ios@users.noreply.github.com>
2021-01-04 20:39:30 +0300

    Добавляет базовую разметку

--- ProfileCard.vue
+++ ProfileCard.vue
@@ -1,9 +1,38 @@
 <template>
-  <h1>Test</h1>
+  <article>
+    <img v-bind:src="avatar" v-bind:alt="alternative" width="96" height="96">
+    <p>{{username}} <span>{{age}}</span></p>
+    <p>{{city}}</p>
+    <dl>
+      <dt>Followers</dt>
+      <dd>{{followers}}</dd>
+      <dt>Likes</dt>
+      <dd>{{likes}}</dd>
+      <dt>Photos</dt>
+      <dd>{{photos}}</dd>
+    </dl>
+  </article>
 </template>
 
 <script>
-export default {};
+export default {
+  data: function () {
+    return {
+      username: 'Victor Crest',
+      age: '26',
+      city: 'London',
+      followers: '80K',
+      likes: '803K',
+      photos: '1.4K',
+      avatar: 'images/image-victor.jpg'
+    };
+  },
+  computed: {
+    alternative: function () {
+      return 'Photo by ' + this.username;
+    }
+  }
+};
 </script>
 
 <style scoped>
