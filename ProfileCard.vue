9d72444ea6460aff34512e758331e735bcca86e4
dmitry-ios <60504537+dmitry-ios@users.noreply.github.com>
2021-01-05 19:05:17 +0300

    Стилизует компонент

--- ProfileCard.vue
+++ ProfileCard.vue
@@ -45,4 +45,100 @@ export default {
 
 <style scoped>
 
+.profile-card {
+  margin: 0 auto;
+  margin-top: 173px;
+  padding-top: 88px;
+  width: 350px;
+  min-height: 286px;
+
+  text-align: center;
+
+  background-color: var(--primary-white);
+  background-image: url('~./public/images/bg-pattern-card.svg');
+  background-position: 0 0;
+  background-size: 350px 140px;
+  background-repeat: no-repeat;
+  border-radius: 12px;
+  box-shadow: 1px 2px 1px 0 var(--primary-very-dark-desaturated-blue);
+}
+
+@media (max-width: 375px) {
+  .profile-card {
+    margin-top: 149px;
+    width: 326px;
+  }
+}
+
+.profile-card__avatar-wrapper {
+  margin: 0 auto;
+  width: 96px;
+  height: 96px;
+
+  border: 5px solid var(--primary-white);
+  border-radius: 50%;
+  overflow: hidden;
+}
+
+.profile-card__avatar-image {
+  width: 96px;
+  height: 96px;
+  display: block;
+}
+
+.profile-card__title,
+.profile-card__item-value {
+  font-weight: bold;
+  color: var(--primary-very-dark-desaturated-blue);
+}
+
+.profile-card__age,
+.profile-card__city {
+  color: var(--neutral-dark-gray);
+}
+
+.profile-card__age {
+  font-weight: normal;
+}
+
+.profile-card__title {
+  margin: 0;
+  padding-top: 20px;
+
+  letter-spacing: 0.5px;
+}
+
+.profile-card__city {
+  margin: 0;
+  padding-top: 13px;
+
+  font-size: 14px;
+  line-height: 14px;
+}
+
+.profile-card__list,
+.profile-card__item-value {
+  margin: 0;
+}
+
+.profile-card__list {
+  margin-top: 23px;
+  padding: 27px 24px;
+  padding-bottom: 20px;
+  display: flex;
+
+  border-top: 1px solid var(--neutral-dark-gray);
+}
+
+.profile-card__item {
+  width: 33%;
+  display: flex;
+  flex-direction: column-reverse;
+}
+
+.profile-card__item-title {
+  font-size: 13px;
+  line-height: 20px;
+  color: var(--neutral-dark-gray);
+}
 </style>
